061c4c4cc891cd2550cd75d427236220647d917f
majing1in <2533144458@qq.com>
2020-12-25 19:54:28 +0800

    demo 11

---
+++ src/sliding_window/Code11_1208_EqualSubstring.java
@@ -0,0 +1,17 @@
+package sliding_window;
+
+public class Code11_1208_EqualSubstring {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int equalSubstring(String s, String t, int maxCost) {
+        int target = 0;
+        if (s.length() == 0 || t.length() == 0 || maxCost < 1) {
+            return target;
+        }
+
+        return target;
+    }
+}
