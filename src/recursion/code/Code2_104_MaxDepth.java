4024aeeb29181c33215470c15a3ac9252fc010bc
majing1in <2533144458@qq.com>
2020-12-27 16:23:33 +0800

    Demo5 # 补充解题思路...

--- src/recursion/code/Code2_104_MaxDepth.java
+++ src/recursion/code/Code2_104_MaxDepth.java
@@ -3,6 +3,12 @@ package recursion.code;
 public class Code2_104_MaxDepth {
 
     public int maxDepth(TreeNode root) {
-
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
+
 }
---
+++ src/recursion/code/Code3_897_IncreasingBST.java
@@ -0,0 +1,12 @@
+package recursion.code;
+
+/**
+ * @Author: 马同学
+ * @Date: 2020/12/27 0027 15:46
+ */
+public class Code3_897_IncreasingBST {
+
+    public TreeNode increasingBST(TreeNode root) {
+
+    }
+}
