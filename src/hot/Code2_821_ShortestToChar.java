421c3e6cee64836c973214fe4bea2ef801c70a8c
majing1in <2533144458@qq.com>
2021-01-20 23:53:10 +0800

    2020.1.20

--- src/hot/Code2_821_ShortestToChar.java
+++ src/hot/Code2_821_ShortestToChar.java
@@ -1,8 +1,27 @@
 package hot;
 
+import java.util.Arrays;
+
 public class Code2_821_ShortestToChar {
 
-    public int[] shortestToChar(String S, char C) {
-        
+    public static void main(String[] args) {
+        int[] ints = shortestToChar("loveleetcode", 'e');
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public static int[] shortestToChar(String S, char C) {
+        int[] target = new int[S.length()];
+        int first = S.indexOf(C);
+        int second = S.indexOf(C, first + 1) != -1 ? S.indexOf(C, first + 1) : first;
+        for (int i = 0; i < S.length(); i++) {
+            int abs1 = Math.abs(i - first);
+            int abs2 = Math.abs(i - second);
+            target[i] = Math.min(abs1, abs2);
+            if (i == second) {
+                first = second;
+                second = S.indexOf(C, first + 1) != -1 ? S.indexOf(C, first + 1) : first;
+            }
+        }
+        return target;
     }
 }
