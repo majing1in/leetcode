62fdf061f6d32508dc1dbc39598169293e4f9014
majing1in <2533144458@qq.com>
2021-01-22 00:11:48 +0800

    2020.1.22

---
+++ src/hot/Code7_38_CountAndSay.java
@@ -0,0 +1,20 @@
+package hot;
+
+import java.util.Stack;
+
+public class Code7_38_CountAndSay {
+
+    public static void main(String[] args) {
+        String s = countAndSay(3);
+        System.out.println(s);
+    }
+
+
+    public static String countAndSay(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+
+}
