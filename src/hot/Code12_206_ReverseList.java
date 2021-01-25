a6cd24e13132651f5e16ccbc7743374dc59a6c29
majing1in <2533144458@qq.com>
2021-01-25 22:03:02 +0800

    2020.1.25

---
+++ src/hot/Code12_206_ReverseList.java
@@ -0,0 +1,18 @@
+package hot;
+
+import entity.ListNode;
+
+public class Code12_206_ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
