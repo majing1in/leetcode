4c2800334818a0669e309e8189d809279d0b840f
majing1in <2533144458@qq.com>
2021-02-24 19:50:04 +0800

    2021.2.24

--- src/hot/Code17_39_CombinationSum.java
+++ src/hot/Code17_39_CombinationSum.java
@@ -13,44 +13,7 @@ public class Code17_39_CombinationSum {
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         ArrayList<List<Integer>> results = new ArrayList<>();
-        Arrays.sort(candidates);
-        if (candidates.length == 0) {
-            return results;
-        }
-        int left = 0;
-        while (left < candidates.length) {
-            int cur = left;
-            int temp = target - candidates[left];
-            List<Integer> list = new ArrayList<>();
-            if (temp == 0) {
-                list.add(candidates[left]);
-                results.add(list);
-                return results;
-            }
-            if (temp < 0) {
-                break;
-            }
-            list.add(candidates[left]);
-            while (temp > 0) {
-                list.add(candidates[cur]);
-                temp = temp - candidates[cur];
-                if (temp == 0) {
-                    results.add(list);
-                    break;
-                }
-                if (temp < candidates[cur]) {
-                    Integer remove = list.remove(list.size() - 1);
-                    temp = temp + remove - candidates[cur];
-                    if (temp == 0) {
-                        list.add(candidates[cur]);
-                        results.add(list);
-                    } else {
-                        cur++;
-                    }
-                }
-            }
-            left++;
-        }
+        // TODO
         return results;
     }
 
---
+++ src/hot/Code23_22_GenerateParenthesis.java
@@ -0,0 +1,16 @@
+package hot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code23_22_GenerateParenthesis {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        if (n < 1) {
+            return list;
+        }
+
+        return list;
+    }
+}
